624b8e57128d0b10e156fdcea8416a20b4ce3c9d
Skkuhodomo <149789510+Skkuhodomo@users.noreply.github.com>
2024-01-21 14:25:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import streamlit as st
 
-#load_dotenv()
+load_dotenv()
 from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI()
-st.title('인공지능 오늘의 한 마디.')
-st.title('GPT가 꽤 괜찮은 한 마디를 작성합니다.  :sunglasses:')
-content = st.text_input('주제 제시')
+st.title('AI Quote of the Day.')
+st.title('GPT comes up with a pretty cool quote. :sunglasses:')
+content = st.text_input('Topic suggestion')
 
-if st.button('한 마디 요청하기'):
-    result = chat_model.predict(content +"에 관해 한 마디 해(10단어이내)")
+if st.button('Request a Quote'):
+    result = chat_model.predict(content + " in 10 words or less")
     st.write(result)
-    
 
 
 
