3d3638a2285bd582f75f4d59a06628eaf02bff38
andy <beppa2396@g.skku.edu>
2024-01-30 22:25:58 +0900

    clean version

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
+
 def main():
     load_dotenv()
     chat_model = ChatOpenAI()
     st.title('AI Quote of the Day.')
     st.title('GPT comes up with a pretty cool quote. :sunglasses:')
     content = st.text_input('Topic suggestion')
-
     if st.button('Request a Quote'):
         result = chat_model.predict(content + " in 10 words or less")
         st.write(result)
